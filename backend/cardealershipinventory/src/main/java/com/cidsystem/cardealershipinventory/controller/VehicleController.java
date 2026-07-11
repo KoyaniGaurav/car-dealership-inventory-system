50b586be713b1b33b8766e496665467a16bb14ab
KoyaniGaurav <kgauravpratikbhai@gmail.com>
2026-07-11 18:49:21 +0530

    test : Vehicle add test pass controller Co-authored : Codex

--- backend/cardealershipinventory/src/main/java/com/cidsystem/cardealershipinventory/controller/VehicleController.java
+++ backend/cardealershipinventory/src/main/java/com/cidsystem/cardealershipinventory/controller/VehicleController.java
@@ -2,8 +2,12 @@ package com.cidsystem.cardealershipinventory.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cidsystem.cardealershipinventory.entity.Vehicle;
@@ -21,5 +25,11 @@ public class VehicleController {
     @GetMapping
     public List<Vehicle> getAllVehicles() {
         return vehicleService.getAllVehicle();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Vehicle createVehicle(@RequestBody Vehicle vehicle) {
+        return vehicleService.addVehicle(vehicle);
     }
 }
