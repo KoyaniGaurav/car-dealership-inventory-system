173b6ab924f492d273216d4a65288b978f4c7668
KoyaniGaurav <kgauravpratikbhai@gmail.com>
2026-07-11 10:22:27 +0530

    chore:initialize Spring Boot project

---
+++ backend/cardealershipinventory/src/main/java/com/cidsystem/cardealershipinventory/CardealershipinventoryApplication.java
@@ -0,0 +1,13 @@
+package com.cidsystem.cardealershipinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardealershipinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardealershipinventoryApplication.class, args);
+	}
+
+}
